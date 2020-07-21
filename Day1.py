70aa954dbebede034d79d88d41cd70499f114618
Ahn-Ssu <21600402@handong.edu>
2020-07-21 18:38:59 +0900

    tset

---
+++ Day1.py
@@ -0,0 +1,20 @@
+#-*-coding: utf-8-*-
+
+"""파이썬은 객체지향을 강력히 지원하는 대화식 인터프리터 언어 """
+"""파이썬은 실행 시간에 동적으로 자료형을 결정,  자료형 관계 없이 일반화된 코드 작성 가능"""
+
+
+print("Hello World!")
+
+print(4+5)
+print(10/5) # 나눗셈 연산자 
+print(9//5) # 몫 연산자
+
+a = 'My name is'
+n = "anaconda"
+
+print(a)
+print(a.upper())
+print(a.split())
+print(':'.join(['hello', 'from', 'the', 'other','side']))
+print(a+n)
