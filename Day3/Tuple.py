5ba1fab2ac8f6ce780bea5d932b38c22cd84e480
Ahn-Ssu <21600402@handong.edu>
2020-07-23 15:17:04 +0900

    pratice tuple end

--- Day3/Tuple.py
+++ Day3/Tuple.py
@@ -1,5 +1,5 @@
 #튜플은 변경 불가능한 데이터 셋 
-#변경
+#변경해야할 데이터는 리스트에, 변경하지 말아야 할 데이터는 튜플에 저장을 한다. 
 
 #튜플 선언시 주의할 점 
 emptyTuple = () 
@@ -10,3 +10,15 @@ oneSizeTuple = (1,) # 튜플의 길이가 하나인 경우, 반드시 콤마로 
 oneSizeTuple.count(1) # 안쪽에 전달한 인자가 몇개 있는가?
 testTuple.index(2) # 2의 좌표좀 
 testTuple.index(2,1) # 2를 검색하는데 위치는 1번 부터 
+
+#튜플 <> 리스트 캐스팅 
+L = list(oneSizeTuple)
+T = tuple(L)
+
+#튜플은 메소드에서 전달 값이 2개 이상일때 활용이 된다 
+def calculate(a,b):
+        return a+b, a-b, a*b, a/b
+add, sub, multi, div = calculate(5,10)
+
+args = (4,7)
+calculate(*args)
