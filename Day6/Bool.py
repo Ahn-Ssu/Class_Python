c5bd9d3bf56b62c8424c2df8a596410a146ec67a
Ahn-Ssu <21600402@handong.edu>
2020-08-23 15:50:19 +0900

    created new concept

---
+++ Day6/Bool.py
@@ -0,0 +1,14 @@
+# 참
+tureStr = "python"
+tureList = [12,3,4]
+tureTuple = (2,3,4,5)
+tureDictionary = {~~~}
+tureNumer = 1
+
+# 거짓
+falseStr = ""
+falseList =[]
+falseTuple = ()
+falseDictionary = {}
+flaseNumber = 0
+None 
