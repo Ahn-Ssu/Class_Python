e7e84706f196a013190296df8e692a0a5f6ce130
Ahn-Ssu <21600402@handong.edu>
2020-09-14 11:45:56 +0900

    changed repo name

---
+++ pythonWeek3/week2Hw.py
@@ -0,0 +1,20 @@
+# 파이썬 2주차 과제
+
+# 3번
+
+Affiliation = input()
+Year = int(input())
+print(“나는 %d년도에 %s에 입학했습니다”)
+print(“나는 “, year, “년도에 “, affiliation, “에 입학했습니다”)
+
+# 4번
+
+Number1 = float(input(“첫 번째 실수 입력 : “))
+Number2 = float(input(“두 번째 실수 입력 : “))
+print(number1 + number2)
+
+
+# 5번
+
+repeatNumber = int(input(“반복 횟수 입력 : “))
+print(“*”*repateNumber)
