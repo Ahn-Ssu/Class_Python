fcd75775451ece9abd791c146bb4649384fb6255
Ahn-Ssu <21600402@handong.edu>
2020-09-24 15:02:00 +0900

    tested function location

--- pythonWeek3/week3KMooc3.py
+++ pythonWeek3/week3KMooc3.py
@@ -3,9 +3,13 @@ myBelong = input("당신의 소속기관을 입력하세요; ")
 myBirhyear = int(input("당신의 출생년도를 입력하세요; "))
 
 
-print()
-print("당신의 이름은 {}".format(myName))
-print("{}에 소속되어 있으시군요".format(myBelong))
-print("당신의 나이는 {}세 맞죠?".format(2020-myBirhyear+1))
+printff(myName,myBelong,myBirhyear)
+
+def printff(name,belong,birthyear):
+    print("당신의 이름은 {}".format(name))
+    print("{}에 소속되어 있으시군요".format(belong))
+    print("당신의 나이는 {}세 맞죠?".format(2020-birthyear+1))
+
+
 
 
