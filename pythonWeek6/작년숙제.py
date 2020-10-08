bb2720d5c8c266df09c9559b02bfae7a22ecbf1a
AhnSsu <suhyun@AhnSsuui-MacBook-Pro.local>
2020-10-08 10:46:30 +0900

    index랑 같이 하기!

--- pythonWeek6/작년숙제.py
+++ pythonWeek6/작년숙제.py
@@ -2,8 +2,9 @@
 majorList = ["GLS", "경영경제", "국제어문", "법학부", "언론정보", "상담복지", "생명과학", "공간환경", "전산전자", "콘융디","기계제어","ICT"]
 
 
+print("="*85)
 for indexNumber, major in zip(range(1,13),majorList):
-    print("{:>2} {:<}".format(indexNumber,major), end=" | ")
+    print("{:>2}) {:<}".format(indexNumber,major), end=" | ")
+    if indexNumber == 6: print()
 print()
 
-    
