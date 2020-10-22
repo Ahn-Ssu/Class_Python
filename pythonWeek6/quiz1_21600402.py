b8236ac5c999ccdb067106fc7b345ad41329fa3a
AhnSsu <suhyun@AhnSsuui-MacBook-Pro.local>
2020-10-23 01:32:27 +0900

    퀴즈봄

---
+++ pythonWeek6/quiz1_21600402.py
@@ -0,0 +1,33 @@
+import turtle 
+
+def flower(t, n ,r, angle):
+    for i in range(n):
+        for i in range (2):
+            t.circle(r,angle)
+            t.left(180-angle)
+        t.left(360/n)
+
+def move(t, length):
+    t.pu()
+    t.fd(length)
+    t.pd()
+
+b = turtle.Pen()
+b.hideturtle()
+b.speed(0)
+
+
+for n in range(7):
+    b.pu()
+    b.goto(-200,200-(n*100))
+    b.pd()
+    for i in range(1,5):
+        if i == 2 :
+            b.color("blue")
+        else:
+            b.color("red")
+        flower(b,4+n,10,180)
+        if i == 4:
+           break
+        move(b,100)
+    
