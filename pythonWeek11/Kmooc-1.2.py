d366669ac5c5d956bf7b1c18b04f1d9f67c6fc82
AhnSsu <suhyun@AhnSsuui-MacBook-Pro.local>
2020-11-10 19:28:29 +0900

    1번 과제 해결

--- pythonWeek11/Kmooc-1.2.py
+++ pythonWeek11/Kmooc-1.2.py
@@ -1,1 +1,18 @@
 #연습하기 3번
+import random
+
+s = 'abcdefghijklmnopqrstuvwxyz'
+base_s = random.sample(s, 26)
+
+password = []
+password_hint = list(zip(s, base_s))
+print(password_hint)
+
+
+input_str = input("문자열 입력: ")
+for letter in input_str:
+    for i in range(26):
+        if letter == password_hint[i][0]:
+            password.append(password_hint[i][1])  
+          
+print(password)
